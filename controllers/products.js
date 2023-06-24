503cc1ba21bc3d9f02abcb3b3cf688b79d8e5058
Anurag Pradhan <anuragp936@gmail.com>
2023-06-24 19:09:37 +0530

    Add pipeline based logic

--- controllers/products.js
+++ controllers/products.js
@@ -3,62 +3,84 @@ const {StatusCodes} = require("http-status-codes");
 const {NotFoundError} = require("../errors");
 
 const getAllProducts = async (req, res) => {
-  const { category, name, sort, fields } = req.query;
-  const queryObject = {};
+    const {category, name, sort, fields} = req.query;
+    const queryObject = {};
 
-  if (category) {
-    queryObject.category = category;
-  }
-  if (name) {
-    queryObject.name = {
-      $regex: name,
-      $options: 'i',
-    };
-  }
+    if (category) {
+        queryObject.category = category;
+    }
+    if (name) {
+        queryObject.name = {
+            $regex: name,
+            $options: 'i',
+        };
+    }
 
-  let result = Product.find(queryObject);
-  if (sort) {
-    const sortList = sort.split(',').join('');
-    result = result.sort(sortList);
-  } else {
-    result = result.sort('name');
-  }
+    const projectionObject = {};
+    if (fields) {
+        const fieldsList = fields.split(',');
+        for (let field of fieldsList) {
+            projectionObject[field] = 1;
+        }
+    }
 
-  if (fields) {
-    const fieldsList = fields.split(',').join(' ');
-    console.log(fieldsList);
-    result = result.select(fieldsList);
-  }
+    const sortObject = {};
+    if (sort) {
+        const sortList = sort.split(',');
+        for (let item of sortList) {
+            let key = item;
+            let value = 1;
+            if (item.startsWith('-')) {
+                key = item.slice(1);
+                value = -1;
+            }
+            sortObject[key] = value;
+        }
+    } else {
+        sortObject.name = 1;
+    }
 
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+    //Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-  const products = await result.skip(skip).limit(limit);
+    const products = await Product.aggregate([
+        {$match: queryObject},
+        ...(Object.keys(projectionObject).length > 0 ? [{$project: projectionObject}] : []),
+        {$sort: sortObject},
+        {$skip: skip},
+        {$limit: limit}
+    ]);
 
-  res.status(StatusCodes.OK).json({
-    products,
-    nbHits: products.length,
-    page,
-    limit,
-  });
+    res.status(StatusCodes.OK).json({
+        products,
+        nbHits: products.length,
+        page,
+        limit,
+    });
 };
 
 const getProduct = async (req, res) => {
-  const {
-    params: { id: productId },
-  } = req
+    const {
+        params: {id: productId},
+    } = req
 
-  const product = await Product.findOne({
-    id: productId,
-  })
-  if (!product) {
-    throw new NotFoundError(`No product with id ${productId}`)
-  }
-  res.status(StatusCodes.OK).json({ product })
+    const product = await Product.aggregate([
+        {
+            $match: {
+                id: Number(productId),
+            },
+        },
+    ]);
+
+    if (product.length === 0) {
+        throw new NotFoundError(`No product with id ${productId}`)
+    }
+    res.status(StatusCodes.OK).json({product})
 }
 
 module.exports = {
-  getAllProducts,
-  getProduct
+    getAllProducts,
+    getProduct
 };
